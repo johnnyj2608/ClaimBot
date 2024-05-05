1f5f2e7c017a8e260d5426514c1abdd4178c5d04
Johnny Jiang <johnnyj2608@gmail.com>
2024-05-05 19:47:41 -0400

    Disabled & Enabling of Buttons

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from customtkinter import CTkToplevel
 from tkcalendar import Calendar
 from excel import validateExcelFile
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from PIL import Image
 
 class ClaimbotGUI:
@@ -33,20 +33,28 @@ class ClaimbotGUI:
         self.browseButton = ctk.CTkButton(master=self.frame, text="Select Excel File", command=self.browseFolder)
         self.browseButton.grid(row=2, column=0, columnspan=5, pady=(0, 10), padx=10)
 
+        self.insuranceLabel = ctk.CTkLabel(master=self.frame, text="Insurance Companies")
+        self.insuranceLabel.grid(row=3, column=0, columnspan=5, pady=(10, 0), padx=10, sticky="ew")
+
+        self.insuranceCombo = ctk.CTkComboBox(master=self.frame, values=list([]))
+        self.insuranceCombo.grid(row=4, column=0, columnspan=5, pady=0, padx=10)
+        self.insuranceCombo.set("")
+        self.insuranceCombo.configure(state="disabled")
+
         self.startDateLabel = ctk.CTkLabel(master=self.frame, text="Start Date")
-        self.startDateLabel.grid(row=4, column=0, columnspan=5, pady=(10, 0), padx=10, sticky="ew")
+        self.startDateLabel.grid(row=5, column=0, columnspan=5, pady=(10, 0), padx=10, sticky="ew")
 
         self.startMonthEntry = ctk.CTkEntry(master=self.frame, width=30)
-        self.startMonthEntry.grid(row=5, column=0, pady=0, padx=1, sticky="e")
-        self.startMonthEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateMonth), "%P"))
+        self.startMonthEntry.grid(row=6, column=0, pady=0, padx=1, sticky="e")
+        self.startMonthEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateMonth), "%P"), state="disabled")
 
         self.startDayEntry = ctk.CTkEntry(master=self.frame, width=30)
-        self.startDayEntry.grid(row=5, column=1, pady=0, padx=1, sticky="e")
-        self.startDayEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateDay), "%P"))
+        self.startDayEntry.grid(row=6, column=1, pady=0, padx=1, sticky="e")
+        self.startDayEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateDay), "%P"), state="disabled")
 
         self.startYearEntry = ctk.CTkEntry(master=self.frame, width=45)
-        self.startYearEntry.grid(row=5, column=2, pady=0, padx=1, sticky="e")
-        self.startYearEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateYear), "%P"))
+        self.startYearEntry.grid(row=6, column=2, pady=0, padx=1, sticky="e")
+        self.startYearEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateYear), "%P"), state="disabled")
 
         img = ctk.CTkImage(dark_image=Image.open(r"./assets/datePickerIcon.png"))
         self.startDatePickerButton = ctk.CTkButton(self.frame, 
@@ -54,47 +62,42 @@ class ClaimbotGUI:
                                                     text="", 
                                                     command=lambda: self.toggleCalendar('start'),
                                                     width=32, 
-                                                    height=32)
-        self.startDatePickerButton.grid(row=5, column=4, pady=0, padx=(5, 10), sticky="w")
+                                                    height=32, 
+                                                    state="disabled")
+        self.startDatePickerButton.grid(row=6, column=4, pady=0, padx=(5, 10), sticky="w")
 
         self.endDateLabel = ctk.CTkLabel(master=self.frame, text="End Date")
         self.endDateLabel.grid(row=7, column=0, columnspan=5, pady=(10, 0), padx=10, sticky="ew")
 
         self.endMonthEntry = ctk.CTkEntry(master=self.frame, width=30)
         self.endMonthEntry.grid(row=8, column=0, pady=0, padx=1, sticky="e")
-        self.endMonthEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateMonth), "%P"))
+        self.endMonthEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateMonth), "%P"), state="disabled")
 
         self.endDayEntry = ctk.CTkEntry(master=self.frame, width=30)
         self.endDayEntry.grid(row=8, column=1, pady=0, padx=1, sticky="e")
-        self.endDayEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateDay), "%P"))
+        self.endDayEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateDay), "%P"), state="disabled")
 
         self.endYearEntry = ctk.CTkEntry(master=self.frame, width=45)
         self.endYearEntry.grid(row=8, column=2, pady=0, padx=1, sticky="e")
-        self.endYearEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateYear), "%P"))
+        self.endYearEntry.configure(validate="key", validatecommand=(self.frame.register(self.validateYear), "%P"), state="disabled")
 
         self.endDatePickerButton = ctk.CTkButton(self.frame, 
                                                     image=img, 
                                                     text="", 
                                                     command=lambda: self.toggleCalendar('end'),
                                                     width=32, 
-                                                    height=32)
+                                                    height=32, 
+                                                    state="disabled")
         self.endDatePickerButton.grid(row=8, column=4, pady=0, padx=(5, 10), sticky="w")
 
-        self.insuranceLabel = ctk.CTkLabel(master=self.frame, text="Insurance Companies")
-        self.insuranceLabel.grid(row=9, column=0, columnspan=5, pady=(10, 0), padx=10, sticky="ew")
-
-        self.insuranceCombo = ctk.CTkComboBox(master=self.frame, values=list([]))
-        self.insuranceCombo.grid(row=10, column=0, columnspan=5, pady=0, padx=10)
-        self.insuranceCombo.set("")
-
         self.autoSubmit = ctk.BooleanVar()
-        self.checkbox = ctk.CTkCheckBox(master=self.frame, text="Enable auto submit", variable=self.autoSubmit)
-        self.checkbox.grid(row=11, column=0, columnspan=5, pady=20, padx=50)
+        self.autoSubmitCheckbox = ctk.CTkCheckBox(master=self.frame, text="Enable auto submit", variable=self.autoSubmit, state="disabled")
+        self.autoSubmitCheckbox.grid(row=9, column=0, columnspan=5, pady=20, padx=50)
 
-        self.browseButton = ctk.CTkButton(master=self.frame, text="Automate", command=self.automate)
-        self.browseButton.grid(row=12, column=0, columnspan=5, pady=0, padx=10)
+        self.automateButton = ctk.CTkButton(master=self.frame, text="Automate", command=self.automate, state="disabled")
+        self.automateButton.grid(row=10, column=0, columnspan=5, pady=0, padx=10)
 
-        self.statusLabel = ctk.CTkLabel(master=self.frame, text="wjabndajwubdwuaj")
+        self.statusLabel = ctk.CTkLabel(master=self.frame, text="")
         self.statusLabel.grid(row=13, column=0, columnspan=5, pady=0, padx=10)
 
         self.frame.grid_columnconfigure((0, 4), weight=1)
@@ -115,8 +118,10 @@ class ClaimbotGUI:
                 self.insuranceCombo.set(sheets[0])
                 fileName = os.path.basename(self.filePath)
                 self.folderLabel.configure(text=fileName, text_color="gray84")
+                self.enableUserInteraction()
             else:
                 self.folderLabel.configure(text="Invalid Excel Template", text_color="red")
+                self.disableUserInteraction()
 
     def toggleCalendar(self, range):
         calendarWindow = CTkToplevel()
@@ -217,15 +222,56 @@ class ClaimbotGUI:
         
         if endDate < startDate:
             return False, "End date set before start date"
+    
+        if (endDate - startDate).days > 60:
+            return False, "Range of dates are too large"
 
-        return True, "Success"
+        return True, "Automating"
 
     def automate(self):
         valid, response = self.validateDateRange()
         if not valid:
-            self.statusLabel.configure(text=response)
+            self.statusLabel.configure(text=response, text_color="red")
             return
-        print("automate")
+
+        self.statusLabel.configure(text=response, text_color="gray84")
+        # Disable buttons & change button color to red 'stop'
+        # Get all members
+        # Use Selenium to fill claims
+        # If autoSubmit = true, submit claims
+        # Enable buttons & revert button color change
+
+    def disableUserInteraction(self):
+        self.insuranceCombo.configure(state="disabled")
+
+        self.startMonthEntry.configure(state="disabled")
+        self.startDayEntry.configure(state="disabled")
+        self.startYearEntry.configure(state="disabled")
+        self.startDatePickerButton.configure(state="disabled")
+
+        self.endMonthEntry.configure(state="disabled")
+        self.endDayEntry.configure(state="disabled")
+        self.endYearEntry.configure(state="disabled")
+        self.endDatePickerButton.configure(state="disabled")
+
+        self.autoSubmitCheckbox.configure(state="disabled")
+        self.automateButton.configure(state="disabled")
+
+    def enableUserInteraction(self):
+        self.insuranceCombo.configure(state="normal")
+
+        self.startMonthEntry.configure(state="normal")
+        self.startDayEntry.configure(state="normal")
+        self.startYearEntry.configure(state="normal")
+        self.startDatePickerButton.configure(state="normal")
+
+        self.endMonthEntry.configure(state="normal")
+        self.endDayEntry.configure(state="normal")
+        self.endYearEntry.configure(state="normal")
+        self.endDatePickerButton.configure(state="normal")
+
+        self.autoSubmitCheckbox.configure(state="normal")
+        self.automateButton.configure(state="normal")
 
     def run(self):
         self.root.mainloop()
