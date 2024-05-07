0f27cbcba9db7ebbc5350241ad2089cd61c8a400
Johnny Jiang <johnnyj2608@gmail.com>
2024-05-06 23:01:32 -0400

    Explicit wait

--- officeAlly.py
+++ officeAlly.py
@@ -17,6 +17,8 @@ dates = getDatesFromWeekdays(4,2024,[0, 1, 2, 3, 4])
 office_ally = 'https://www.officeally.com/secure_oa.asp'
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -24,32 +26,49 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 driver.get(office_ally)
 driver.maximize_window()
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
 
 username = 'username'
 password = 'password'
 
-username_field = driver.find_element('xpath', '//*[@id="username"]')
+username_field = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(('xpath', '//*[@id="username"]'))
+)
+password_field = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(('xpath', '//*[@id="password"]'))
+)
+login_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(('xpath', '/html/body/main/section/div/div/div/form/div[2]/button'))
+)
+
 username_field.send_keys(username)
-
-password_field = driver.find_element('xpath', '//*[@id="password"]')
 password_field.send_keys(password)
-
-login_button = driver.find_element('xpath', '/html/body/main/section/div/div/div/form/div[2]/button')
 login_button.click()
 
 driver.get('https://www.officeally.com/secure_oa.asp?GOTO=OnlineEntry&TaskAction=Manage')
 
-iframe = driver.find_element('xpath', '//*[@id="Iframe9"]')
+iframe = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(('xpath', '//*[@id="Iframe9"]'))
+)
 driver.switch_to.frame(iframe)
 
-create_claim_button = driver.find_element('xpath', '//*[@id="Button2"]')
+# Insert summary data here
+
+create_claim_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(('xpath', '//*[@id="Button2"]'))
+)
 create_claim_button.click()
 
 driver.switch_to.default_content()
-iframe = driver.find_element('xpath', '//*[@id="Iframe9"]')
+iframe = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(('xpath', '//*[@id="Iframe9"]'))
+)
 driver.switch_to.frame(iframe)
 
-add_row_button = driver.find_element('xpath', '//*[@id="btnAddRow"]')
+add_row_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable(('xpath', '//*[@id="btnAddRow"]'))
+)
 for i in range(len(dates)-12):
     add_row_button.click()
+
+# Insert member days data here
