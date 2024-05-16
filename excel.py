db4f1ee1439dad50a618880a8e47b2a680ac5e53
Johnny Jiang <johnnyj2608@gmail.com>
2024-05-16 00:18:44 -0400

    Included Payer and Form in summary

--- excel.py
+++ excel.py
@@ -5,6 +5,8 @@ import pandas as pd
 def validateExcelFile(excelFilePath):
     app = xw.App(visible=False)
 
+    closeExcelFile(excelFilePath)
+
     memberSheet = None
     summaryValues = {}
 
@@ -22,34 +24,38 @@ def validateExcelFile(excelFilePath):
 
         if summary and summary.range('B2').value == 'Professional (CMS)':
             summaryValues = {
+                "form": summary.range('B2').value,
                 "insurance": summary.range('B3').value,
                 "username": summary.range('B4').value,
                 "password": summary.range('B5').value,
-                "billingProvider": summary.range('B6').value,
-                "renderingProvider": summary.range('B9').value,
-                "facilities": summary.range('B10').value,
-                "servicePlace": summary.range('B11').value,
-                "cptCode": summary.range('B12').value,
-                "modifier": summary.range('B13').value,
-                "diagnosis": summary.range('B14').value,
-                "charges": summary.range('B15').value,
-                "units": summary.range('B16').value,
+                "payer": summary.range('B6').value,
+                "billingProvider": summary.range('B7').value,
+                "renderingProvider": summary.range('B10').value,
+                "facilities": summary.range('B11').value,
+                "servicePlace": summary.range('B12').value,
+                "cptCode": summary.range('B13').value,
+                "modifier": summary.range('B14').value,
+                "diagnosis": summary.range('B15').value,
+                "charges": summary.range('B16').value,
+                "units": summary.range('B17').value,
             }
         elif summary and summary.range('B2').value == 'Institutional (UB)':
             summaryValues = {
+                "form": summary.range('B2').value,
                 "insurance": summary.range('B3').value,
                 "username": summary.range('B4').value,
                 "password": summary.range('B5').value,
-                "billingProvider": summary.range('B6').value,
-                "physician": summary.range('B9').value,
-                "revenueCode": summary.range('B10').value,
-                "description": summary.range('B11').value,
-                "cptCodeSDC": summary.range('B12').value,
-                "cptCodeTrans": summary.range('B13').value,
-                "chargesSDC": summary.range('B14').value,
-                "chargesTrans": summary.range('B15').value,
-                "unitsSDC": summary.range('B16').value,
-                "unitsTrans": summary.range('B17').value,
+                "payer": summary.range('B6').value,
+                "billingProvider": summary.range('B7').value,
+                "physician": summary.range('B10').value,
+                "revenueCode": summary.range('B11').value,
+                "description": summary.range('B12').value,
+                "cptCodeSDC": summary.range('B13').value,
+                "cptCodeTrans": summary.range('B14').value,
+                "chargesSDC": summary.range('B15').value,
+                "chargesTrans": summary.range('B16').value,
+                "unitsSDC": summary.range('B17').value,
+                "unitsTrans": summary.range('B18').value,
             }
         else:
             return [], {}
@@ -76,12 +82,12 @@ def validateExcelFile(excelFilePath):
 def recordClaims(insurance, start, end, claims):
     pass
 
-# def closeExcelFile(excelFilePath):
-#     for proc in psutil.process_iter():
-#         try:
-#             if 'EXCEL.EXE' in proc.name():
-#                 for item in proc.open_files():
-#                     if excelFilePath.lower() in item.path.lower():
-#                         proc.kill()
-#         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-#             pass
+def closeExcelFile(excelFilePath):
+    for proc in psutil.process_iter():
+        try:
+            if 'EXCEL.EXE' in proc.name():
+                for item in proc.open_files():
+                    if excelFilePath.lower() in item.path.lower():
+                        proc.kill()
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
