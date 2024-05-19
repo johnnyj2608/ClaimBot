48ea1cf928bec3b63184484eb625e3710b564501
Johnny Jiang <johnnyj2608@gmail.com>
2024-05-18 23:32:46 -0400

    Rename via pdf name instead of most recent

--- claimForms/cmsScript.py
+++ claimForms/cmsScript.py
@@ -277,29 +277,33 @@ def cmsDownload(driver, autoDownload, memberName, stopFlag):
     )
     driver.switch_to.frame(iframe)
 
+    pdfName = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(('xpath', '//*[@id="main-message"]/h1')))
+    pdfName = pdfName.text
+
     openButton = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable(('xpath', '//*[@id="open-button"]'))
+        EC.element_to_be_clickable(('xpath', '//*[@id="open-button"]'))
     )
     if stopProcess(stopFlag): return
     openButton.click()
 
-    time.sleep(2)
-
     pdfList = glob.glob(os.path.join(autoDownload, '*.pdf'))
+    pdfName = os.path.join(autoDownload, pdfName)
+    while pdfName not in pdfList:
+        if stopProcess(stopFlag): return
+        time.sleep(1)
+        pdfList = glob.glob(os.path.join(autoDownload, '*.pdf'))
+
     renamedPDF = f'{memberName}.pdf'
     renamedPDF = os.path.join(autoDownload, renamedPDF)
 
-    if renamedPDF in pdfList:
-        suffix = 2
-        while True:
-            renamedPDF = f'{memberName} {suffix}.pdf'
-            renamedPDF = os.path.join(autoDownload, renamedPDF)
-            if renamedPDF not in pdfList:
-                break
-            suffix += 1
-
-    else:
+    suffix = 2
+    while renamedPDF in pdfList:
+        if stopProcess(stopFlag): return
+        renamedPDF = f'{memberName} {suffix}.pdf'
         renamedPDF = os.path.join(autoDownload, renamedPDF)
+        if renamedPDF not in pdfList:
+            break
+        suffix += 1
 
-    curPDF = max(pdfList, key=os.path.getctime)
-    os.rename(curPDF, renamedPDF)
+    os.rename(pdfName, renamedPDF)
